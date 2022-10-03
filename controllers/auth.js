2326e0d2d1e7e80ee9cc13ae7d7680e8cae79938
JacobRosey <jrosey77@gmail.com>
2022-10-03 17:05:51 -0400

    auth.js fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,12 +2,14 @@ const mysql = require('mysql');
 //const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
+/*
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
-});
+});*/
+const db = mysql.createConnection(process.env.JAWSDB_URL);
 
 exports.register = (req, res) => {
     console.log(req.body);
