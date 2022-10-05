de0b2a54a92d92d3a9c03857e71a1d1ff6811a96
JacobRosey <jrosey77@gmail.com>
2022-10-05 16:06:12 -0400

    http

--- public/index.js
+++ public/index.js
@@ -228,7 +228,7 @@ function ajaxFunc(key, path, method, d) {
         var response = xhr.responseText;
         if (response == "Login Successful!") {
             //Will need to change route when this site is live
-            window.location.replace('https://livedatabase.herokuapp.com/database');
+            window.location.replace('http://livedatabase.herokuapp.com/database');
             alert(response);
             //Setup session storage
             sessionStorage.setItem('logged', true);
